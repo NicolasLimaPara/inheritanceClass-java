c421dd3db29d3e5522b3da461a03ad6307c3927c
Clidecir Nicolas <c.nicolaslima@hotmail.com>
2022-07-21 21:06:12 -0300

    Polimorfism

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,21 +9,16 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Account acc1 = new Account( 1001, "Jason", 1000.0);
+		Account x = new Account (1020, "Alex", 1000.0);
+		Account y = new SavingsAccount (1023, "Maria", 1000.0, 0.01);
 		
-		acc1.withdraw(200.0);
-		System.out.println(acc1.getBalance());
-	
-		Account acc2 = new SavingsAccount(1002, "Mary", 1000.0, 0.01);
-		acc2.withdraw(200.0);
-		System.out.println(acc2.getBalance());
-	
-		Account acc3 = new BankAccount(1003, "Daniel", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
 		
-		//test git new computer
+		x.withdraw(50.0);
+		y.withdraw(50.0);
 		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
+						
 	}
 
 }
